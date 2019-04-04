d06669eb1774ad7767e19036ed282bf4135d2d94
Jacek Podkanski <ruby.object@googlemail.com>
2019-04-04 20:36:04 +0100

    spa page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'spa/index'
+  get 'spa/*index', to: 'spa#index'
+  get 'spa/', to: 'spa#index'
+
   resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
